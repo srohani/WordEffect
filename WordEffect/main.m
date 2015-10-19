bdb340a1c27385248eee6fa549ab676f33f88cfc
srohani <srohani@gmail.com>
2015-10-19 16:39:01 -0700

    Initial Commit

---
+++ WordEffect/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WordEffect
+//
+//  Created by Samer Rohani on 2015-10-19.
+//  Copyright © 2015 Samer Rohani. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
